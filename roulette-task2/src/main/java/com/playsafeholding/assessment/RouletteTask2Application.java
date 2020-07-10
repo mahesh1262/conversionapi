0dfd3c74e4c91cf5e52e04c23205f809b166a3b3
unknown <bmahesh1262@gmai.com>
2020-07-10 14:43:36 +0200

    first commit

---
+++ roulette-task2/src/main/java/com/playsafeholding/assessment/RouletteTask2Application.java
@@ -0,0 +1,13 @@
+package com.playsafeholding.assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouletteTask2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouletteTask2Application.class, args);
+	}
+
+}
