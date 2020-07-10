514a6b6d3a0fcd8b1b33e3f5c408482a1c5fe60f
unknown <bmahesh1262@gmai.com>
2020-07-10 13:37:12 +0200

    first commit

---
+++ rest-service-task1/src/test/java/com/playsafeholding/assessment/RestServiceTask1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playsafeholding.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServiceTask1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
