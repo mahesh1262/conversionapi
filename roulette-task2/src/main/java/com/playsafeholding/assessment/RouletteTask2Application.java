16a56b5c4d46b5b0c6efb727ef1f8291a399ec83
unknown <bmahesh1262@gmai.com>
2020-07-10 15:09:55 +0200

    main class file

--- roulette-task2/src/main/java/com/playsafeholding/assessment/RouletteTask2Application.java
+++ roulette-task2/src/main/java/com/playsafeholding/assessment/RouletteTask2Application.java
@@ -1,13 +1,27 @@
 package com.playsafeholding.assessment;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @SpringBootApplication
-public class RouletteTask2Application {
+public class RouletteTask2Application implements CommandLineRunner {
+	
+	private static final Logger logger = LoggerFactory.getLogger(RouletteTask2Application.class);
 
 	public static void main(String[] args) {
+		logger.info("Starting roulette application");
 		SpringApplication.run(RouletteTask2Application.class, args);
+		
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		new Round().playGame();
+		
 	}
 
 }
